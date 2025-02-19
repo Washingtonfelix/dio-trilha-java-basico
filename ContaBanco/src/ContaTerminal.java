0ca76c42a70eadf396d211a15d05200a6754be11
washington <felix2001-1@hotmail.com>
2025-02-19 17:46:03 -0300

    Cirado o app da ContaTerminal

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+    
+            System.out.println("Por favor, digite o número da Conta !");
+            int numeroConta = scanner.nextInt();
+    
+            System.out.println("Por favor, digite o número da Agência !");
+            int numeroAgencia = scanner.nextInt();
+    
+            System.out.println("Por favor, digite seu nome !");
+            String nome = scanner.next();
+    
+            System.out.println("Por favor, digite o seu sobrenome !");
+            String sobrenome = scanner.next();
+    
+            System.out.println("Por favor, digite o seu saldo !");
+            double saldo =  scanner.nextDouble();
+    
+            System.out.println("Ola, " + nome + " " + sobrenome + ", obrigado por cirar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponivel para saque");
+            
+        
+    }
+    
+}
