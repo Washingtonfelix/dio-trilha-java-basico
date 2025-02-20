faa15947d257672bc905a588c326255dbe2a4f95
washington <felix2001-1@hotmail.com>
2025-02-20 16:40:46 -0300

    Aplicação para contar uma determinado quantidade de numeros e mostrar somente os numeros impares

---
+++ ContarNumerosPares/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        for (int numero = 0; numero <=200; numero ++){
+            if(numero %2 == 0 )
+            continue;
+
+            System.out.println(numero);
+        }
+
+   }     
+}
