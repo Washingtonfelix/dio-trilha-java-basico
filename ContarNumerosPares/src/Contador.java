ebc769d548d8678f5bcdb8fe5deda669c0044526
washington <felix2001-1@hotmail.com>
2025-02-20 16:41:20 -0300

    Alterado o nome do arquivo

--- ContarNumerosPares/src/Contador.java
+++ ContarNumerosPares/src/Contador.java
@@ -1,4 +1,4 @@
-public class App {
+public class Contador {
     public static void main(String[] args) throws Exception {
         for (int numero = 0; numero <=200; numero ++){
             if(numero %2 == 0 )
